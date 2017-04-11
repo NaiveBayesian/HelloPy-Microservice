da6425687220a0c36e4d1d65b4ea6338f862c28c
NaiveBayesian <emersonct911@gmail.com>
2017-04-10 19:08:25 -0600

    Added custom hello via url path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Much of the code below is taken shamelessly from https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
+# Built on top of code from https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 # HTTPRequestHandler class
@@ -8,13 +8,18 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Send response status code
         self.send_response(200)
-
+        
         # Send headers
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+        # Parse path to customize response
+        name = "world"
+        if(len(self.path) > 1):
+            name = self.path[1:]
+            
         # Send message back to client
-        message = "Hello world!"
+        message = f"Hello {name}!"
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
@@ -22,13 +27,12 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 # Define the global 'run' function for our server
 def run(port):
     print(f"Starting server on port {port}...")
-
+    
     #Server settings
     server_address = ('127.0.0.1', port)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print("Running server...")
-    httpd.serve_forever()
-
+    httpd.handle_request()
 
 # Now run our new server!
 run(8081)
